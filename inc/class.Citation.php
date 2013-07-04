d39fa90b299b160e318a36b9f4829a58e2442c83
Jenny Sharps <jsharps85@gmail.com>
2013-07-04 11:59:42 -0400

    Fix citations post ype

--- inc/class.Citation.php
+++ inc/class.Citation.php
@@ -21,7 +21,7 @@ class Citation {
     public $citationMeta;
         
     function __construct( $file ){
-        add_action( 'admin_init', array( __CLASS__, 'createPostType' ) );
+        add_action( 'init', array( __CLASS__, 'createPostType' ) );
         add_action( 'save_post', array( __CLASS__, 'savePost' ) );
         
         require_once( dirname( $file ) . '/inc/class.TemplateRenderer.php' );
@@ -34,44 +34,38 @@ class Citation {
      * @author Jenny Sharps <jsharps85@gmail.com>
      */
     public static function createPostType() {
-        register_post_type( self::$postTypeName, // (http: //codex.wordpress.org/Function_Reference/register_post_type)
-	 	 // let's now add all the options for this post type
-		array(
-                    'labels' => array(
-			'name' => __('Citations', 'post type general name'), // This is the Title of the Group
-			'all_items' => __('All Citations'),
-			'singular_name' => __('Citation', 'post type singular name'), // This is the individual type
-			'add_new' => __('Add New', 'custom post type item'), // The add new menu item
-			'add_new_item' => __('Add New Citation'), // Add New Display Title
-			'edit' => __( 'Edit' ), // Edit Dialog
-			'edit_item' => __('Edit Citation'), // Edit Display Title
-			'new_item' => __('New Citation'), // New Display Title
-			'view_item' => __('View Citation'), // View Display Title
-			'search_items' => __('Search Citations'), // Search Custom Type Title 
-			'not_found' =>  __('No citations found in the database.'), // This displays if there are no entries yet 
-			'not_found_in_trash' => __('Nothing found in Trash'), // This displays if there is nothing in the trash
-			'parent_item_colon' => ''
-                    ), // end of arrays
-                    'description' => __( 'This is a content type for citations.' ), // Custom Type Description
-                    'public' => true,
-                    'publicly_queryable' => true,
-                    'exclude_from_search' => false,
-                    'show_ui' => true,
-                    'show_in_menu' => true,
-                    'query_var' => true,
-                    'menu_position' => 8, // this is what order you want it to appear in on the left hand side menu 
-                    'menu_icon' => get_stylesheet_directory_uri() . '/library/images/custom-post-icon.png', // the icon for the custom post type menu
-                    'rewrite' => array( 'slug' => 'citations', 'with_front' => false ),
-                    'capability_type' => 'post',
-                    'hierarchical' => false,
-                    'permalink_epmask' => 'EP_PERMALINK & EP_YEAR', 
-                    'has_archive' => 'citations',
-                    'register_meta_box_cb' => array( __CLASS__, 'addMetaBoxes' ),
-                    // the next one is important, it tells what's enabled in the post editor
-                    'supports' => array( 'title', 'thumbnail', 'excerpt', 'revisions', 'sticky'),
-                    'taxonomies' => array('post_tag')
-	 	) // end of options
-	); // end of register post type
+        $labels = array(
+            'name' => 'Citations',
+            'singular_name' => 'Citation',
+            'add_new' => 'Add New',
+            'add_new_item' => 'Add New Citation',
+            'edit_item' => 'Edit Citation',
+            'new_item' => 'New Citation',
+            'all_items' => 'All Citation',
+            'view_item' => 'View Citation',
+            'search_items' => 'Search Citations',
+            'not_found' =>  'No citations found',
+            'not_found_in_trash' => 'No citations found in Trash', 
+            'parent_item_colon' => '',
+            'menu_name' => 'Citations'
+          );
+
+          $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true, 
+            'show_in_menu' => true, 
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'citation' ),
+            'capability_type' => 'post',
+            'hierarchical' => false,
+            'menu_position' => 10,
+            'register_meta_box_cb' => array( __CLASS__, 'addMetaBoxes' ),
+            'supports' => array( 'title', 'author', 'thumbnail', 'excerpt', 'sticky' )
+          );
+
+          register_post_type( self::$postTypeName, $args );
         
     }
     
